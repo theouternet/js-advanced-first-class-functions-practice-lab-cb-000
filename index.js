816366bffbbb631fdfb14b68b2abb1db1cb3f38a
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-05-22 18:19:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file!
+
+const logDriverNames = function(drivers) {
+  drivers.forEach(function(driver) {
+    console.log(driver.name);
+  });
+}
+
+
+
+const logDriversByHometown = function(drivers, location) {
+  drivers.forEach(function(driver) {
+  console.log(driver.name) if driver.location === location
+  })
+}
+
