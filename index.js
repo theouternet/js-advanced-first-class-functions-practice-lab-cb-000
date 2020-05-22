3e9dd23626bdcd995063403d0ef02fa29396fb58
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-05-22 18:31:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ const driversByRevenue = function(drivers) {
 
 const driversByName = function(drivers) {
   return drivers.slice().sort(function(one, two) {
-    return one.revenue - two.revenue;
+    return one.revenue + two.revenue;
   });
 }
